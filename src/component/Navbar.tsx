6e58616b981840a5ef991f4895342374ead4157c
ImamBaihaqi <imam.baihaqi2396@gmail.com>
2023-12-14 16:10:19 +0700

    fix icon navbar

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
+import iconSearch from '../../public/icon/search.png';
 
 const Navbar = () => {
   return (
     <div className="bg-[#4F4F4F] text-white h-10 p-4 flex items-center">
-      <img
-        src="../../public/icon/search.png"
-        alt="Search Icon"
-        className="w-4"
-      />
+      <img src={iconSearch} alt="Search Icon" className="w-4" />
     </div>
   );
 };
